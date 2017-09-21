6ab230a0a18095456c1cd3b2acdc1dc3f6d17e5a
arencrg <clarence13cariaga@gmail.com>
2017-09-21 16:00:23 +0800

    Day 2 Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import { fetchWeather } from "./fetchWeather.js";
 
 class App extends Component {
   render() {
@@ -13,37 +12,31 @@ class App extends Component {
   }
 }
 
-class WeatherWeatherLang extends Component {
 
+class WeatherWeatherLang extends Component {
   constructor(props) {
     super(props);
     this.state = {
       city: "Manila",
-      country: "ph",
+      weatherData: [],
+      myURL1: "http://api.openweathermap.org/data/2.5/weather?q=",
+      myURL2: "&appid=3c50495593ac9632d01ab38da3c87495"
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   getWeather(city = this.state.city) {
-
-    const myURL1 = city => "http://api.openweathermap.org/data/2.5/weather?q="
-    const myURL2 = "&appid=3c50495593ac9632d01ab38da3c87495"
-  //  const weatherUrl = `myURL1{this.state.city},{this.state.country}myURL2`
-    const weatherUrl = city => `myURL1{city},{this.state.country}myURL2`
-
-        fetch(weatherUrl(this.state.city))
+    const weatherUrl = `${this.state.myURL1}${this.state.city}${this.state.myURL2}`
+        fetch(weatherUrl)
           .then(response => {
             if (!response.ok) {
               throw Error("Network request failed")
             }
-
-            return response
+            return response => response.json()
           })
-          .then(d => d.json())
-          .then(d => {
-            this.setState({
-              weatherData: d
-            })
+          .then(weatherData => {
+            this.setState({weatherData: weatherData})
+            console.log(weatherData);
           })
       }
 
@@ -54,15 +47,17 @@ class WeatherWeatherLang extends Component {
   }
 
     render() {
+      const weatherUrl = `${this.state.myURL1}${this.state.city}${this.state.myURL2}`
       return (
         <div>
           <h1>{this.state.city}</h1>
-          <h3>Display the weather data (from the api) here</h3>
+          <p> Data taken from <a href = {weatherUrl} target="_blank">here</a></p>
+          <p>Display the weather data (from the api) here (in a pretty little table or something)</p>
+          <p> {this.state.weatherData} </p>
 
-
-          <h4>Do you want to try a new city?</h4>
+<hr/>
+          <p>Do you want to try a new city?</p>
             <form onSubmit={this.handleSubmit}>
-                <p>Do you want to try a new city?</p>
                 <input className="form-input" ref="locationName" type="text"/>
                 <input type="submit" value="Submit" onChange={this.handleSubmit} />
             </form>
